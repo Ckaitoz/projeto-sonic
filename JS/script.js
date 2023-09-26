1c7f62f922622023e43dce77c1f3aff312b244b4
Ckaitoz <caio.felipevs@gmail.com>
2023-09-26 16:14:56 -0300

    projeto finalizado

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+const sonic = document.querySelector('.sonic')
+const esp = document.querySelector('.espinho')
+
+const jump = () => {
+    sonic.classList.add('jump')
+
+    setTimeout(() => {
+        sonic.classList.remove('jump')
+    },500)
+}
+
+const loop = setInterval(() => {
+
+    const espPosition = esp.offsetLeft
+    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '')
+
+    if(espPosition <= 120 && espPosition > 0 && sonicPosition < 80){
+        esp.style.animation = 'none'
+        esp.style.left = `${espPosition}px`
+
+        sonic.style.animation = 'none'
+        sonic.style.bottom = `${espPosition}px`
+
+        sonic.src = 'image/game-over.webp'
+        sonic.style.width = '150px'
+        sonic.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+},10)
+
+document.addEventListener('keydown',jump)
