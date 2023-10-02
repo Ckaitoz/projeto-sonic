adc0040d46f9245de52eb069b6bc1e5d0cdb8b85
Ckaitoz <caio.felipevs@gmail.com>
2023-10-02 16:08:38 -0300

    não sei oq eu fiz

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,8 @@ const esp = document.querySelector('.espinho')
 
 const jump = () => {
     sonic.classList.add('jump')
-
+    
+    
     setTimeout(() => {
         sonic.classList.remove('jump')
     },500)
@@ -14,6 +15,11 @@ const loop = setInterval(() => {
     const espPosition = esp.offsetLeft
     const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '')
 
+    if(sonicPosition > 0){
+        sonic.src = 'image/spinning.gif'
+    }else{
+        
+    }
     if(espPosition <= 120 && espPosition > 0 && sonicPosition < 80){
         esp.style.animation = 'none'
         esp.style.left = `${espPosition}px`
@@ -24,9 +30,14 @@ const loop = setInterval(() => {
         sonic.src = 'image/game-over.webp'
         sonic.style.width = '150px'
         sonic.style.marginLeft = '50px'
-
+        
         clearInterval(loop)
+        
     }
+
+    
 },10)
+
+
 
 document.addEventListener('keydown',jump)
